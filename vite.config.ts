cdc51f24b40b7880079fc7cc5b0d3b3e320504d2
studentSystem <albertojunior848423803@gmail.com>
2024-06-16 13:26:13 -0700

    Final setup for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/Dev-Albertojunior1/',
+   base: '/frontend-email-sender/'
 })
